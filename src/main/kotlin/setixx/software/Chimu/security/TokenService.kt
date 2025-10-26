19e906d08a8aecea44883cc2673fdd02210df29b
setixx <s7tixxx@gmail.com>
2025-10-26 15:33:41 +0300

    Introduce Token Service and Refresh Token Repository:
    - Rename `JwtUtil` to `TokenService` and enhance JWT functionality with claims extraction and improved secret handling.
    - Add `RefreshTokenRepository` for managing token-user mappings in memory.
    - Downgrade JJWT library to version `0.11.5` for compatibility.
    - Update `build.gradle.kts` dependencies accordingly.

---
+++ src/main/kotlin/setixx/software/Chimu/security/TokenService.kt
@@ -0,0 +1,40 @@
+package setixx.software.Chimu.security
+
+import io.jsonwebtoken.Claims
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.io.Decoders
+import io.jsonwebtoken.security.Keys
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
+import java.security.Key
+import java.util.Date
+
+@Component
+class TokenService(
+    @Value("\${JWT_SECRET}") private val secret: String,
+    @Value("\${JWT_EXPIRATION}") private val expirationTime: Long,
+){
+    private val secretKey: Key = Keys.hmacShaKeyFor(Decoders.BASE64.decode(secret))
+
+    fun generateToken(subject: String, expiration: Date, additionalClaims: Map<String, Any> = emptyMap()): String {
+        return Jwts.builder()
+            .setClaims(additionalClaims)
+            .setSubject(subject)
+            .setIssuedAt(Date(System.currentTimeMillis()))
+            .setExpiration(expiration)
+            .signWith(secretKey)
+            .compact()
+    }
+
+    fun extractUsername(token: String): String {
+        return extractAllClaims(token).subject
+    }
+
+    private fun extractAllClaims(token: String): Claims {
+        return Jwts.parserBuilder()
+            .setSigningKey(secretKey)
+            .build()
+            .parseClaimsJws(token)
+            .body
+    }
+}
