667df0c9e1d77ffc056756f008f4fa64f08577f7
setixx <s7tixxx@gmail.com>
2026-04-13 22:26:13 +0300

    Add configurable registration availability
    - Introduce `isSignUpOpen` configuration property in `RegistrationService` to control user sign-up access.
    - Add a conditional check within the `register` method of `RegistrationService` to prevent new user registrations when the `SIGNUP_OPEN` property is `false`.
    - Throw an `IllegalArgumentException` if a registration attempt occurs while sign-up is disabled.
    - Configure `RegistrationService` to inject the `SIGNUP_OPEN` environment variable using Spring's `@Value` annotation.

--- api/src/main/kotlin/software/setixx/chimu/api/service/RegistrationService.kt
+++ api/src/main/kotlin/software/setixx/chimu/api/service/RegistrationService.kt
@@ -1,5 +1,6 @@
 package software.setixx.chimu.api.service
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
 import software.setixx.chimu.api.domain.User
@@ -11,10 +12,14 @@ import java.time.Instant
 @Service
 class RegistrationService(
     private val userRepository: UserRepository,
-    private val passwordEncoder: PasswordEncoder
+    private val passwordEncoder: PasswordEncoder,
+    @Value("\${SIGNUP_OPEN}") private val isSignUpOpen: Boolean = true,
 ) {
     fun register(email: String, rawPassword: String): User {
         val encodedPassword = passwordEncoder.encode(rawPassword)
+        if (!isSignUpOpen) {
+            throw IllegalArgumentException("Registration is temporarily unavailable.")
+        }
         if (userRepository.findByEmail(email) != null) {
             throw EmailAlreadyExistsException("User with email $email already exists")
         }
